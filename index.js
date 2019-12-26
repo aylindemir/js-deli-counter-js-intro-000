27bf7c38e1d9b7d2f610e00d2d808b1fb6730e44
Aylin Demir <aylin.de@outlook.com>
2019-12-26 23:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
+var katzDeliLine = [];
 function takeANumber(lineOfPeople,newPersonName){
   var lineNumber = lineOfPeople+1
   return `Welcome, ${newPersonName}. You are number ${lineNumber} in line.`
-lineOfPeople=lineNumber
+  lineOfPeople=lineNumber
+  katzDeliLine.push(newPersonName)
 }
