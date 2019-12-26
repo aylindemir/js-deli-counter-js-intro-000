58883f7e51a3e4a145e8e7223b14b762b9670c2a
Aylin Demir <aylin.de@outlook.com>
2019-12-26 23:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function takeANumber(lineOfPeople,newPersonName){
   var lineNumber = lineOfPeople+1
   return `Welcome, ${newPersonName}. You are number ${lineNumber} in line.`
-
+lineOfPeople=lineNumber
 }
